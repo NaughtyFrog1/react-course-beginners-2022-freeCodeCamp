061d7b19b61ad1a6ec5e21cfba3576de85a523d7
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-03-04 17:25:23 -0300

    render jokes dynamically

--- joke-app/src/App.js
+++ joke-app/src/App.js
@@ -1,30 +1,11 @@
 import React from 'react'
 import Joke from './components/Joke'
+import jokes from './data/jokes.data'
 
 export default function App() {
   return (
     <main className="jokes-container">
-      <Joke
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-      />
-      <Joke
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-      />
-      <Joke punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally." />
-      <Joke
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-      />
-      <Joke
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm"
-      />
-      <Joke
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-      />
+      {jokes.map((joke) => <Joke {...joke} />)}
     </main>
   )
 }
