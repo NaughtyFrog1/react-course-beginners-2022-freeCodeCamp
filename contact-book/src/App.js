0c242ca576444ee2f8cd6b1dc297e4914dc2c32c
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-03-03 22:46:37 -0300

    show contacts in App

--- contact-book/src/App.js
+++ contact-book/src/App.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import Contact from './components/Contact'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className="contacts">
+      <Contact
+        image="mr-whiskerson.png"
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact
+        image="fluffykins.png"
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <Contact
+        image="felix.png"
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
+      <Contact
+        image="pumpkin.png"
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@scrimba.com"
+      />
+    </div>
   )
 }
